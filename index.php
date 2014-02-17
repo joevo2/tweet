9e4a7298e546c103ae0cfeabca6de86bf7fed09f
Joel Yek <joevo2@gmail.com>
2014-02-17 20:55:15 +0800

    Added CREATE TABLE & INSERTION

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 	<body>
 		<h1>Twitter App Clone</h1>
 		<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>">
+			<!--To post on the same page, action="submit.php" to redirect to other page -->
 
 			Name:<input type="text" name="name"><br>
 			Post:<br>
@@ -15,13 +16,40 @@
 				echo "Posted by: " . $_POST["name"];
 			}
 
+			//establish connection
 			$con = mysqli_connect("localhost","user_1","123456","tweet_db");
 
+			//check connection
 			if (mysqli_connect_errno()) {
 				echo "Failed to connect to MySQL: " . mysqli_connect_error();
 			}
+			echo "<br>Succesfully connected to MySQL<br>";
 
-			echo "<br>Succesfully connected to MySQL";
+			//To create Database
+			/*
+			$sql = "CREATE DATABASE my_db";
+			if (mysqli_query($con,$sql)) {
+				echo "Database created succesfully";
+			}
+			else {
+				echo "Error Creating database" . mysqli_error($con);
+			}
+			*/
+
+			//Create Table
+			$sql = "CREATE TABLE Tweet(
+				PID INT NOT NULL AUTO_INCREMENT,
+				PRIMARY KEY(PID),
+				User CHAR(30),
+				Post CHAR(120)
+				)";
+			if (mysqli_query($con,$sql)) {
+				echo "Table created succesfully";
+			}
+			else {
+				echo "Error creating table " . mysqli_error($con);
+			}
+
 
 		?>
 	</body>
